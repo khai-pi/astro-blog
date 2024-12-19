126f65b78e5dd3cb021b0fe4cf4a3abbe1d69db8
khai-pi <khaiphamhoang.pi@gmail.com>
2024-12-19 17:29:01 +0100

    add greeting and script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://fastidious-babka-0c5e40.netlify.app"
+  site: "https://fastidious-babka-0c5e40.netlify.app",
+  integrations: [preact()]
 });
